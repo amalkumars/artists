b3ac71e3575645b73e2b4c39fb19a3b84d9e2447
Amal <amal9994kumar@gmail.com>
2017-03-19 01:40:05 +0530

    To add search feature spec

--- spec/features/artists/search_spec.rb
+++ spec/features/artists/search_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
-
+# This feature spec is to test applications entire flow
+# User searches artists based on country
+# User gets a list of all artists
+# User can select an artist to see this top tracks
 describe 'the location based artist search', type: :feature do
   context 'when keyword is blank' do
     it 'will show no results found' do
+      # When user searches without a country name our method is expected to return
+      # => 'No Records Found.' message
       VCR.use_cassette('feature_artists_search_blank') do
         visit '/artists/search'
         fill_in 'common-search', with: ''
@@ -14,6 +19,8 @@ describe 'the location based artist search', type: :feature do
 
   context 'when keyword is a valid country' do
     it 'will show artists' do
+      # When user enters a valid country name our method is expected to return
+      # => List of artists and links to their Top tracks page.
       VCR.use_cassette('feature_artists_search_valid') do
         visit '/artists/search'
         fill_in 'common-search', with: 'spain'
@@ -24,10 +31,30 @@ describe 'the location based artist search', type: :feature do
         have_link('David Bowie', href: '/artists/5441c29d-3602-4898-b1a1-b77fa23b8e50/tracks')
       end
     end
+
+    it 'navigate to top artists listing page' do
+      # From search results page user can navigate to top tracks of an aritsit
+      allow_any_instance_of(Lastfm::ArtistTopTracksService).to receive(:find).and_return({ tracks: [ ArtistTrack.new({
+        name: "Ziggy Stardust",
+        playcount: "4228918",
+        listeners: "715147",
+        mbid: "b1b29af7-2190-4d0f-9aa8-fe397105679c",
+        url: "https://www.last.fm/music/David+Bowie/_/Ziggy+Stardust"
+        })] })
+      VCR.use_cassette('feature_artists_search_valid') do
+        visit '/artists/search'
+        fill_in 'common-search', with: 'spain'
+        click_button 'Search'
+        click_link('David Bowie')
+        expect(page).to have_content 'Ziggy Stardust'
+      end
+    end
   end
 
   context 'when keyword is a invalid country' do
     it 'will show no results found' do
+      # When user searches an invalid country name our method is expected to return
+      # => 'country param invalid' message
       VCR.use_cassette('feature_artists_search_invalid') do
         visit '/artists/search'
         fill_in 'common-search', with: 'xxx222@'
